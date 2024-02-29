251c67be1651dc2fb642e3ef31cec399c6096c61
ihop <ihopkinsc26@gmail.com>
2024-02-29 15:00:46 -0600

    test

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function onHover(event){
 function resetText(){
     text.innerHTML = "hi, this is my page. I am learning html/css/js right now. Most of what you see here will be subject to change.<br><br> Hover over one of my projects on the left to see what I'm up to. woop";
 }                    
-resetText();
+
 const illinix = document.getElementById("illinix");
 illinix.description = "Completed in December 2023 as a final project for ECE391 (Computer Systems Programming). Click to be brought to the emulator.";
 illinix.addEventListener("mouseover",onHover);
@@ -23,5 +23,7 @@ const website = document.getElementById("website");
 website.description = "Congrats, you're already here!";
 website.addEventListener("mouseover",onHover);
 website.addEventListener("mouseleave",resetText);
+
+resetText();
 
 
