5e6dd0cac6461f41a3784ff8b2a48dbf77cdcf8f
ihop <ihopkinsc26@gmail.com>
2024-02-29 14:54:31 -0600

    updates

--- scripts.js
+++ scripts.js
@@ -5,20 +5,23 @@ function onHover(event){
 }
 
 function resetText(){
-    text.innerHTML = "hi, this is my page. I am learning html/css/js right now. Most of what you see here right will be subject to change.<br><br> Hover over one of my projects on the left to see what I'm up to.";
+    text.innerHTML = "hi, this is my page. I am learning html/css/js right now. Most of what you see here will be subject to change.<br><br> Hover over one of my projects on the left to see what I'm up to. woop";
 }                    
+resetText();
 const illinix = document.getElementById("illinix");
-illinix.addEventListener("mousemove",onHover);
+illinix.description = "Completed in December 2023 as a final project for ECE391 (Computer Systems Programming). Click to be brought to the emulator.";
+illinix.addEventListener("mouseover",onHover);
 illinix.addEventListener("mouseleave",resetText);
-illinix.description = "Descriptions in progress... Click to see emulated OS";
 
 const dotcollector = document.getElementById("dot");
+dotcollector.description = "Click to be taken to the github page where you can learn more.";
 dotcollector.addEventListener("mouseover",onHover);
 dotcollector.addEventListener("mouseleave",resetText);
-dotcollector.description = "Click to be taken to the github page where you can learn more.";
+
 
 const website = document.getElementById("website");
+website.description = "Congrats, you're already here!";
 website.addEventListener("mouseover",onHover);
 website.addEventListener("mouseleave",resetText);
-website.description = "3, descriptions TODO";
+
 
